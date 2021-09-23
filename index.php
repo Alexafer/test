4bb221420439b2f2f251604aff41f71f44daca55
Alexafer <brintire@gmail.com>
2021-09-23 22:21:48 +0300

    ll

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-<?php 
-$uuu = 999;
-echo = $uuu;
-
-?>
-<li>rrrrr</li>
-
-
+    <?php
+    $tyuu = 9998;
+    echo  $tyuu;
+    echo $tyuu + 19999;
+    ?> 
 </body>
-
 </html>
