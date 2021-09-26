f53b68383a238e57a42f4bce52923e077c58857c
Alexafer <brintire@gmail.com>
2021-09-26 03:16:29 +0300

    kk

--- index.php
+++ index.php
@@ -7,10 +7,18 @@
     <title>Document</title>
 </head>
 <body>
+    <script>
+let $number_arrey = [9900, 87, 33];
+if (1 != 10)
+for (let $num of $number_arrey) {
+let $arifmetic = $num * 1;
+console.log($arifmetic);
+}
+    </script>
     <?php
-    $tyuu = 9998;
+    $tyuu = 1;
     echo  $tyuu;
-    echo $tyuu + 19999;
-    ?> 
+    echo $tyuu + 199990000;
+    ?>
 </body>
 </html>
