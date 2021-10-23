9242ea82730dcd3daa8cab6ca56405f90693079c
Alexafer <brintire@gmail.com>
2021-10-23 11:26:52 +0300

    lesson-1

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 // let messageButton = document.getElementById('menu-complited-tasks');
-alert('я подключил скрипт?');
-// console.log(document.getElementById('menu-complited-tasks'));
-// let messageButton = document.querySelector('.tasks-complited');
-let message = "Are you sure you want to change the number of tasks?";
-let taskOpen = document.querySelector('.tasks-open');
-let taskComplited = document.querySelector('.tasks-complited');
-let imgCollection = document.querySelectorAll('.my-upload img');
-// confirm(confirmm);
-// messageButton.onclick = function() {
-//         if (confirm(message) == true) {
-//             taskOpen.textContent = taskOpen--;
-//             taskComplited.textContet = taskComplited++;
-//         } else if (confirm(message) == true && taskOpen == 0) {
-//             alert("Not have open task");
-//         }
-//     }
-// for (let imgColl of imgCollection) {
-//     imgColl.onclick() = function() {
+// alert('я подключил скрипт-1');
+let element = document.querySelector('.menu-complited-tasks'),
+    tasksComplited = document.querySelector('.tasks-complited'),
+    setTasksComplited = tasksComplited.textContent,
+    numTaskComplited = Number(setTasksComplited),
+    tasksOpen = document.querySelector('.tasks-open'),
+    setTaskOpen = tasksOpen.textContent,
+    numTaskOpen = Number(setTaskOpen),
+    message = "Are you sure you want to change the number of tasks?";
 
-//     }
-// }
-let num = document.querySelector('.num');
+element.onclick = function() {
+    if (confirm(message) && numTaskOpen > 0) {
+        numTaskComplited = numTaskComplited + 1;
+        tasksComplited.textContent = numTaskComplited;
+        numTaskOpen = numTaskOpen - 1;
+        tasksOpen.textContent = numTaskOpen;
+    } else if (numTaskOpen == 0) {
+        alert("Not have open task!");
+    }
+}
+let res = document.querySelector('.num');
 let arrElem = document.querySelectorAll('.my-upload img');
 let arrayElem = [];
 
-for (var i = 0; i < arrElem.length; i++) {
+for (let i = 0; i < arrElem.length; i++) {
     arrayElem.push(arrElem[i]);
     arrElem[i].addEventListener('click', function(e) {
-        num.innerHTML = arrayElem.indexOf(e.target);
+        res.innerHTML = arrayElem.indexOf(e.target);
     });
 }
