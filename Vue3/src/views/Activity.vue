d1592e1d32bbaabb731de8a957866a726e115f06
Alexafer <brintire@gmail.com>
2021-12-04 12:54:17 +0300

    lesson-7

--- Vue3/src/views/Activity.vue
+++ Vue3/src/views/Activity.vue
@@ -20,7 +20,7 @@
           img(:src="require('@/assets/3.png')" alt="")
         p.main-li-text
           | {{ fMessage.text }}
-          img(v-for="(item, index) in fMessage.collectionImg" :key="index" v-on:click="notificFunction(index)" :src="require('@/assets/' + item + '.jpg')" width="100" height="100" alt=" ")
+          img(v-for="(item, index) in fMessage.collectionImg" :key="index" v-on:click="this.$emit('notificationIndex', index)" :src="require('@/assets/' + item + '.jpg')" width="100" height="100" alt=" ")
         p.main-time {{ fMessage.time }}
   div
 </template>
@@ -34,11 +34,11 @@ export default defineComponent ({
       required: true
     }
   },
-  methods: {
-    notificFunction(index: number) {
-      this.$emit('notificationIndex', index);
-    },
-  },
+  // methods: {
+  //   notificFunction(index: number) {
+  //     this.$emit('notificationIndex', index);
+  //   },
+  // },
 });
 </script>
 <style lang=""></style>
